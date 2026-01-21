086eb85e1d87a8ce6a76f415172a079b184ecf76
Mehmet Kılınçkaya <mehmet@solvapps.com>
2026-01-21 15:45:14 +0300

    init function change.

--- Sources/DrapeSDK/DrapeViewController.swift
+++ Sources/DrapeSDK/DrapeViewController.swift
@@ -28,7 +28,8 @@ public class DrapeViewController: UIViewController {
     var selectedCategory: DrapeCategory = .upperBody
     
     public init() {
-        super.init(nibName: nil, bundle: nil)
+        let frameworkBundle = Bundle(for: DrapeViewController.self)
+        super.init(nibName: "DrapeViewController", bundle: frameworkBundle)
     }
     
     required init?(coder: NSCoder) {
